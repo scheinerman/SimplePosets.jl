1ab3031d68fd0de2b7d139e357e07508d23c5e58
Ed Scheinerman <ers@jhu.edu>
2014-10-04 20:27:45 -0400

    New functionality

--- README.md
+++ README.md
@@ -28,10 +28,18 @@ Basic inspection
 * `card(P)` returns the cardinality of `P` (number of elements).
 * `relations(P)` returns a list of all pairs `(x,y)` with `x<y` in
   this poset.
+* `incomparables(P)` returns a list of all incomparable pairs. If
+  `(x,y)` is listed, we do not also list `(y,x)`. 
 * `has(P,x)` determine if `x` is an element of `P`.
 * `has(P,x,y)` determine if `x<y` in the poset `P`.
 * `above(P,x)` returns a list of all elements above `x` in `P`.
 * `below(P,x)` returns a list of all elements below `x` in `P`.
+* `maximals(P)` returns a list of the minimal elements of `P`.
+* `minimals(P)` returns a list of the minimal elements of `P`.
+* `check(P)` returns `true` provided the internal data structures of
+  `P` are valid and `false` otherwise. **Note**: There should be no
+  reason to use this function if the poset is created and manipulated
+  by the functions provided in this module.
 
 Constructors
 ------------
@@ -39,13 +47,54 @@ Constructors
 * `Antichain(n)` creates an antichain with elements `1:n`
 * `Antichain(list)` creates an antichain with elements drawn from
   `list`, a one-dimensional array.
+* `Boolean(n)` creates the subsets of an `n`-set poset in which
+  elements are named as `n`-long binary strings.
 * `Chain(n)` creates a chain with elements `1:n` in which
   `1<2<3<...<n`.
 * `Chain(list)` creates a chain with elements drawn from `list` (in that
   order) in.
+* `Divisors(n)` creates the poset whose elements are the divisors of
+  `n` ordered by divisibility. 
+* `RandomPoset(n,d)` creates a random `d`-dimensional poset on `n`
+  elements. 
+* `StandardExample(n)` creates the canonical `n`-dimensional poset
+  with `2n` elements in two layers. The lower layer elements are named
+  from `-1` to `-n` and the upper layer from `1` to `n`. We have
+  `-i<j` exactly when `i!=j`.
+
+Operations
+----------
+
+* `inv(P)` creates the inverse poset of `P`, i.e., we have `x<y` in
+  `P` iff we have `y<x` in `inv(P)`. 
+* `intersect(P,Q)` creates the intersection of the two posets (which
+  must be of the same element type). Typically the two posets have the
+  same elements, but this is not necessary. The resulting poset's
+  elements is the intersection of the two element sets, and relations
+  in the result are those relations common to both `P` and `Q`.
+* `P*Q` is the Cartesian product of the two posets.
+
+Poset properties
+----------------
+
+Really need width, height here. But for now
+
+* `ComparabilityGraph(P)` returns a `SimpleGraph` whose vertices are
+  the elements of `P` and in which two distinct vertices are adjacent
+  iff they are comparable in `P`.
+* `mobius(P)` creates the Mobius function for this poset (as a
+  dictionary from pairs of elements to `Int` values).
+* `mobius_matrix(P)` is the inverse of `zeta_matrix(P)`.
+* `zeta(P)` creates the zeta function for this poset (as a dictionary
+  from pairs of elements to `Int` values). We have `(x,y) ==> 1`
+  provided `x==y` or `x<y`, and `(x,y) ==> 0` otherwise. 
+* `zeta_matrix(P)` produces the zeta matrix. Order of elements is the
+  same as produced by `elements(P)`.
+
 
 To Do
 -----
 
-Intersection of posets, maximal/minimal elements, cover relations,
-standard posets `Boolean(n)` and others.
+cover relations, height, width, maxchain(s), maxantichain(s),
+chain cover, antichain cover (easier)
+relabel
--- src/SimplePosets.jl
+++ src/SimplePosets.jl
@@ -2,21 +2,31 @@ module SimplePosets
 
 using SimpleGraphs
 
-import Base.show
+import Base.show, Base.isequal, Base.inv, Base.intersect, Base.zeta
 
 import SimpleGraphs.add!, SimpleGraphs.has, SimpleGraphs.delete!
 
-export SimplePoset, elements, relations
-export card, show, add!, has
-export delete! # elements only
+export SimplePoset, check
+export elements, relations, incomparables
+export card, show, add!, has, delete! 
 export above, below
+export maximals, minimals
 
-export Chain, Antichain
+export zeta_matrix, zeta, mobius_matrix, mobius
+
+
+export Chain, Antichain, Divisors, Boolean, StandardExample
+export RandomPoset
+
+export ComparabilityGraph
+
+export inv, intersect
 
 type SimplePoset{T}
     D::SimpleDigraph{T}
     function SimplePoset()
         D = SimpleDigraph{T}()
+        forbid_loops!(D)
         new(D)
     end
 end
@@ -24,11 +34,58 @@ end
 # Create a new poset whose elements have a specific type (default Any)
 SimplePoset(T::DataType=Any) = SimplePoset{T}()
 
+# Validation check. This should not be necessary to ever use if the
+# poset was properly built.
+function check(P::SimplePoset)
+    # cycle detection
+    PP = deepcopy(P)
+    while true
+        bottoms = minimals(PP)
+        if length(bottoms)==0
+            break
+        end
+        for b in bottoms
+            delete!(PP,b)
+        end
+    end
+    if card(PP)>0
+        warn("Cycles detected")
+        return false
+    end
+
+    # transitive closure check 
+    Z = zeta_matrix(P)
+    if countnz(Z) != countnz(Z*Z)
+        warn("Not transitively closed")
+        return false
+    end
+    return true
+end
+
+# Check if two posets are the same
+isequal(P::SimplePoset, Q::SimplePoset) = isequal(P.D,Q.D)
+==(P::SimplePoset, Q::SimplePoset) = isequal(P,Q)
+
 # return a list of the elements in P
 elements(P::SimplePoset) = vlist(P.D)
 
 # return a list of all the < relations in P
 relations(P::SimplePoset) = elist(P.D)
+
+# list all pairs of elements that are incomparable to each other
+function incomparables{T}(P::SimplePoset{T})
+    els = elements(P)
+    n   = length(els)
+
+    pairs = (T,T)[]
+    for j=1:n-1
+        for k=j+1:n
+            push!(pairs, (els[j],els[k]) )
+        end
+    end
+    
+    filter( p -> !has(P,p[1],p[2]) && !has(P,p[2],p[1]) , pairs)
+end
 
 # return the cardinality of this poset
 card(P::SimplePoset) = NV(P.D)
@@ -116,14 +173,12 @@ function Antichain{T}(els::Array{T,1})
     return P
 end
 
-
 # Construct a chain 1<2<3<...<n
 function Chain(n::Int)
     P = Antichain(n)
     for k=1:n
         add!(P,k)
-    end
-    
+    end    
     if n > 1
         for k=1:n-1
             add!(P,k,k+1)
@@ -142,6 +197,263 @@ function Chain{T}(els::Array{T,1})
     return P
 end
 
+# requires n>1, but we don't check. gives first prime factor. this is
+# not exposed.
+function first_prime_factor(n::Int)
+    if isprime(n)
+        return n
+    end
     
+    for k=2:n
+        if n%k == 0
+            return k
+        end
+    end
+end
+
+# creates the set of divisors of a positive integer. should we expose? 
+function divisors(n::Int)
+    if n<1 
+        error("divisors only works on positive integers")
+    end
+
+    if n==1
+        return IntSet(1)
+    end
+
+    p = first_prime_factor(n)
+    if n==p
+        return IntSet(1,p)
+    end
+    
+    A = divisors(div(n,p))
+    Alist = collect(A)
+
+    Blist = [ p*x for x in Alist ]
+
+    B = IntSet(Blist)
+    
+    return union(A,B)
+end
+
+# Create the poset of the divisors of a positive integer
+function Divisors(n::Int)
+    if n<1
+        error("Argument must be a positive integer")
+    end
+
+    A = divisors(n)
+    P = SimplePoset(Int)
+
+    for a in A
+        add!(P,a)
+    end
+
+    for a in A
+        for b in A
+            if a!=b && b%a == 0
+                add!(P,a,b)
+            end
+        end
+    end
+    return P
+end
+
+# Create the Boolean lattice poset. Elements are n-long binary
+# strings.
+function Boolean(n::Int)
+    if n<1
+        error("Argument must be a positive integer")
+    end
+    
+    P = SimplePoset(ASCIIString)
+
+    NN = (1<<n) - 1
+    for e = 0:NN
+        add!(P,bin(e,n))
+    end
+
+    for e = 0:NN
+        for f=0:NN
+            if e!=f && e|f == f
+                add!(P.D,bin(e,n), bin(f,n))
+            end
+        end
+    end
+
+
+    return P
+end
+
+# Helper function for RandomPoset
+function vec_less(x::Array{Float64,1}, y::Array{Float64,1})
+    n = length(x)
+    return all([ x[k] <= y[k] for k=1:n ])
+end
+
+# Create a random d-dimensional poset with n elements
+function RandomPoset(n::Int, d::Int)
+    if n<1 || d<1
+        error("Require n and d positive in RandomPoset(n,d)")
+    end
+    vectors = [ rand(d) for k=1:n ]
+
+    P = SimplePoset(Int)
+    for k=1:n
+        add!(P,k)
+    end
+
+    for i=1:n
+        for j=1:n
+            if i!=j && vec_less(vectors[i],vectors[j])
+                add!(P,i,j)
+            end
+        end
+    end
+    return P
+end
+
+    
+
+
+
+# Create standard example poset. Lower level named by negatives and
+# upper level by positives.
+function StandardExample(n::Int)
+    if n<1
+        error("Argument must be a positive integer")
+    end
+    
+    P = SimplePoset(Int)
+    for e=1:n
+        add!(P,e)
+        add!(P,-e)
+        for f=1:n
+            if e!=f
+                add!(P.D,-e,f)
+            end
+        end
+    end
+    return P
+end
+
+# maximal and minimal elements
+maximals(P::SimplePoset) = filter(x->out_deg(P.D,x)==0, elements(P))
+minimals(P::SimplePoset) = filter(x->in_deg(P.D,x)==0, elements(P))
+
+# The inverse of a poset is a new poset with the order reversed
+function inv{T}(P::SimplePoset{T})
+    Q = SimplePoset(T)
+    for e in P.D.V
+        add!(Q,e)
+    end
+    for r in relations(P)
+        x,y = r[1],r[2]
+        add!(Q.D,y,x)
+    end
+    return Q
+end
+
+# Create the intersection of two posets (must be of same element
+# type). Ideally, the two posets have the same set of elements, but
+# this is not necessary; if they don't we just intersect the element
+# sets first.
+function intersect{T}(P::SimplePoset{T}, Q::SimplePoset{T})
+    R = SimplePoset(T)
+    elist = filter(x -> has(P,x), elements(Q))
+    for e in elist
+        add!(R,e)
+    end
+
+    rlist = filter( r -> has(P,r[1],r[2]), relations(Q))
+    for r in rlist
+        add!(R.D, r[1],r[2])
+    end
+    return R
+end
+
+# Produce the cartesian product of two posets
+function *{S,T}(P::SimplePoset{S}, Q::SimplePoset{T})
+    PQ = SimplePoset{(S,T)}()
+
+    for a in P.D.V
+        for b in Q.D.V
+            add!(PQ,(a,b))
+        end
+    end
+
+    elist = elements(PQ)
+    for alpha in elist
+        for beta in elist
+            if alpha != beta
+                if has(P,alpha[1],beta[1]) || alpha[1]==beta[1]
+                    if has(Q,alpha[2],beta[2]) || alpha[2]==beta[2]
+                        add!(PQ.D, alpha, beta)
+                    end
+                end
+            end
+        end
+    end
+    return PQ
+end
+
+# not much call for this, so we don't expose
+function strict_zeta_matrix(P::SimplePoset)
+ elist = elements(P)
+    n = length(elist)
+    Z = zeros(Int, n, n)
+
+    for i=1:n
+        for j=1:n
+            if has(P,elist[i],elist[j])
+                Z[i,j] = 1
+            end
+        end
+    end    
+    return Z
+end        
+
+# Zeta function as a matrix
+zeta_matrix(P::SimplePoset) = strict_zeta_matrix(P) + int(eye(card(P)))
+   
+# Mobius function as a matrix
+mobius_matrix(P::SimplePoset) = int(inv(zeta_matrix(P)))
+
+# Zeta function as a dictionary
+function zeta{T}(P::SimplePoset{T})
+    z = Dict{(T,T),Int}()
+    els = elements(P)
+    for a in els
+        for b in els
+            if a==b || has(P,a,b)
+                z[a,b] = 1          
+            else
+                z[a,b] = 0
+            end
+        end
+    end
+    return z
+end
+
+# Mobius function of this poset.
+function mobius{T}(P::SimplePoset{T})
+    mu = Dict{(T,T),Int}()
+    els = elements(P)
+    M = mobius_matrix(P)
+    n = length(els)
+
+    for i=1:n
+        a = els[i]
+        for j=1:n
+            b = els[j]
+            mu[a,b] = M[i,j]
+        end
+    end
+    return mu
+end
+
+# The comparability graph of a poset
+ComparabilityGraph(P::SimplePoset) = simplify(P.D)
+               
 end # end of module SimplePosets
 
